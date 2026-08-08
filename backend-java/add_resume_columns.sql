74dc5cda24795e1cf10d9f9f667cc8bc2dea81fe
Thilak-29 <thilakvignesh@gmail.com>
2026-08-08 11:17:01 +0530

    chore: clean codebase, remove unused MySQL dumps and legacy components

--- backend-java/add_resume_columns.sql
+++
@@ -1,1 +0,0 @@
-USE internmatch_db; ALTER TABLE student_profiles ADD COLUMN resume_file_name VARCHAR(255) DEFAULT 'alex_johnson_resume_2025.pdf'; ALTER TABLE student_profiles ADD COLUMN resume_parsed_text TEXT;
